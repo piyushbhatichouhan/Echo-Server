00d42776b0436c2386c705407a93c2ff7cb95f19
piyush-bhati-chouhan <piyushbhatichouhan@gmail.com>
2026-08-04 10:56:45 +0530

    fixed api url in useLogStream.js

--- frontend/src/hooks/useLogStream.js
+++ frontend/src/hooks/useLogStream.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export default function useLogStream(projectId, onLog) {
   useEffect(() => {
@@ -7,7 +8,7 @@ export default function useLogStream(projectId, onLog) {
     const token = localStorage.getItem("echohub_token");
 
     const eventSource = new EventSource(
-      `http://localhost:3000/api/projects/${projectId}/logs/live?token=${token}`,
+      `${API_URL}/api/projects/${projectId}/logs/live?token=${token}`,
     );
 
     eventSource.onmessage = (event) => {
