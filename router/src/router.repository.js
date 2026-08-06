49beab155cce752135b5fdb6cf6d493faa294ba1
piyush-bhati-chouhan <piyushbhatichouhan@gmail.com>
2026-08-06 17:52:31 +0530

    dfhh

--- router/src/router.repository.js
+++ router/src/router.repository.js
@@ -19,7 +19,22 @@ SELECT
     pd.ssl_status,
     pd.verification_status,
     pd.publication_status
-    `,
+
+FROM project_domains pd
+
+JOIN projects p
+ON p.id = pd.project_id
+
+LEFT JOIN deployments d
+ON d.project_id = p.id
+
+WHERE
+    pd.hostname = $1
+AND pd.publication_status = 'active'
+
+ORDER BY d.created_at DESC
+LIMIT 1;
+`,
     [hostname],
   );
   console.log("[Router] Query result:", rows);
