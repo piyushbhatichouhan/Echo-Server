5cdf366607e0ad261dff8642693aa35ec3808a3f
piyush-bhati-chouhan <piyushbhatichouhan@gmail.com>
2026-08-06 17:34:07 +0530

    added log

--- router/src/proxy.service.js
+++ router/src/proxy.service.js
@@ -3,7 +3,9 @@ const httpProxy = require("http-proxy");
 const proxy = httpProxy.createProxyServer({});
 
 const proxyRequest = (req, res, deployment) => {
-  target = `http://${deployment.containerName}:${deployment.containerPort}`;
+  const target = `http://${deployment.containerName}:${deployment.containerPort}`;
+
+  console.log("[Proxy] Target:", target);
 
   proxy.web(req, res, {
     target,
