571ac972e596276d08e2d63a644594f8e93bd9fd
piyush-bhati-chouhan <piyushbhatichouhan@gmail.com>
2026-08-06 10:43:50 +0530

    fixed router port

--- router/server.js
+++ router/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
     await connectDatabase();
 
     app.listen(PORT, () => {
-      console.log(`Echo Router is running on port ${PORT}`);
+      console.log(`Echo Router is running on port this: ${PORT}`);
     });
   } catch (err) {
     console.error(err);
