555f7e229465f7118741177b9c7e6082baa71669
piyush-bhati-chouhan <piyushbhatichouhan@gmail.com>
2026-08-06 12:02:34 +0530

    test commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const startServer = async () => {
   await validateStartup();
 
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`🚀 Server running on port, hello ${PORT}`);
   });
 };
 
